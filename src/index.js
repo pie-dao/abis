bc41b5ef52aa14c33708ce020ee7915bd4feceff
Mick de Graaf <mick@mickdegraaf.nl>
2020-04-01 20:38:29 +0200

    pieSmartPool ABI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import erc20Abi from './abis/erc20';
+import pieSmartPoolAbi from './abis/piesSmartPool';
 
 export const erc20 = erc20Abi;
+export const pieSmartPool = pieSmartPoolAbi;
 
 export default {
   erc20,
+  pieSmartPool
 };
