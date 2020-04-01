4c779664e2575550210b4fd071a26ad7b53653e0
Mick de Graaf <mick@mickdegraaf.nl>
2020-04-01 20:45:48 +0200

    Linting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import erc20Abi from './abis/erc20';
-import pieSmartPoolAbi from './abis/piesSmartPool';
+import pieSmartPoolAbi from './abis/pieSmartPool';
 
 export const erc20 = erc20Abi;
 export const pieSmartPool = pieSmartPoolAbi;
 
 export default {
   erc20,
-  pieSmartPool
+  pieSmartPool,
 };
